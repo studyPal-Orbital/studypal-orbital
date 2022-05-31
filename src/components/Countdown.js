5c1688b4bc01fd5532efe707ec6fbcb82664511a
Carrie Zheng Jiarui <carriezhengjiarui@gmail.com>
2022-05-31 12:27:25 +0800

    Update Countdown.js
    Debugged countdown timer, enlarge size

--- src/components/Countdown.js
+++ src/components/Countdown.js
@@ -5,7 +5,7 @@ class Countdown extends Component {
     state = {
         timerRunning: false, // Is the timer running?
         startTime: 0, // Start time (in milliseconds).
-        totalTime: 0 // Total time (in ms) the timer has been running since start/reset.
+        totalTime: 0 // Total time (in ms) the timer will run.
     };
 
     // Called when timer is started or resumed.
@@ -46,8 +46,9 @@ class Countdown extends Component {
         if (this.state.timerRunning === false) {
             // If timer has stopped running,
             // reset totalTime to startTime.
-            this.setState({
-                totalTime: this.state.startTime
+            this.setState({ 
+                totalTime: 0,
+                startTime: 0
             });
         }
     };
@@ -57,8 +58,8 @@ class Countdown extends Component {
     // Each button checks if the input time is valid, i.e. lies within the time frame 0ms to 24h. 
     // If valid, update totalTime.
     setDuration = input => {
-        const { totalTime, timerRunning } = this.state;
-        const max = 216000000; // 60 hours.
+        const { timerRunning, totalTime } = this.state;
+        const max = 86400000; // 24 hours.
         if (!timerRunning) {
             if (input === "addHours" && totalTime + 3600000 < max) {
                 this.setState({ totalTime: totalTime + 3600000 });
@@ -103,12 +104,13 @@ class Countdown extends Component {
                     {/* Downwards White Arrow &#8681 */}
                     <button class = 'countdown-setTime' onClick={() => this.setDuration("minusHours")}>&#8681;</button>
                     <button class = 'countdown-setTime' onClick={() => this.setDuration("minusMinutes")}>&#8681;</button>
-                    <button class = 'countdown-setTime' onClick={() => this.setDuration("decSeconds")}>&#8681;</button>
+                    <button class = 'countdown-setTime' onClick={() => this.setDuration("minusSeconds")}>&#8681;</button>
                 </div>
             
                 <div class="button-wrapper">
-                    {/* Start - Show button when timer stops running and (start time is 0 or equals total time). */}
-                    {timerRunning === false && (startTime === 0 || totalTime === startTime) && (
+                    {/* Start - Show button when timer is not running and 
+                    (start time is 0, or equals total time, or total time is 0). */}
+                    {timerRunning === false && (startTime === 0 || startTime === totalTime || totalTime === 0) && (
                         <button className="countdown-start" onClick={this.startTimer}>
                             Start
                         </button>
@@ -122,16 +124,16 @@ class Countdown extends Component {
                     )}
                 
                     {/* Resume - Show button when timer is not running and 
-                    (start time is not 0, and not equals total time, and total time not equals 0). */}
+                    (start time > 0, and not equals total time, and total time not equals 0). */}
                     {timerRunning === false && (startTime > 0 && startTime !== totalTime && totalTime !== 0) && (
                         <button className="countdown-start" onClick={this.startTimer}>
                             Resume
                         </button>
                     )}
             
-                    {/* Reset - Show button when (timer is not running or total time < 1 second)
-                    and (start time not equals total time. */}
-                    {(timerRunning === false || totalTime < 1000) && (startTime > 0 && startTime !== totalTime) && (
+                    {/* Reset - Show button when timer is not running and 
+                    (start time > 0, or not equals total time, or total time > 0. */}
+                    {timerRunning === false && (startTime > 0 || startTime !== totalTime || totalTime > 0) && (
                         <button className="countdown-reset" onClick={this.resetTimer}>
                             Reset
                         </button>
