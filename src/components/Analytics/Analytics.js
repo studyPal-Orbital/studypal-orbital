b8b63360b60dcdb267cfed46d7c015bbf1e3a2fa
ongyongen <ongyongen2021@gmail.com>
2022-07-04 22:24:28 +0800

    add tracking for user task completion

--- src/components/Analytics/Analytics.js
+++ src/components/Analytics/Analytics.js
@@ -55,9 +55,9 @@ const Analytics = () => {
                         startDate={new Date('2021-12-31')}
                         endDate={new Date('2022-12-31')}
                         values={[
-                            { date: '2016-01-01', count: 12 },
-                            { date: '2016-01-22', count: 122 },
-                            { date: '2016-01-30', count: 38 },
+                            { date: '2022-01-01', count: 12 },
+                            { date: '2022-01-22', count: 122 },
+                            { date: '2022-01-30', count: 38 },
                         ]}
                     />
                     <div className="goal-setting-section">
@@ -67,9 +67,9 @@ const Analytics = () => {
                             startDate={new Date('2021-12-31')}
                             endDate={new Date('2022-12-31')}
                             values={[
-                                { date: '2016-01-01', count: 12 },
-                                { date: '2016-01-22', count: 122 },
-                                { date: '2016-01-30', count: 38 },
+                                { date: '2022-01-01', count: 12 },
+                                { date: '2022-01-22', count: 122 },
+                                { date: '2022-01-30', count: 38 },
                             ]}
                         />
                     </div>
