1aa448abf835b56833ce3e6ddb5b49d520f5afa5
ongyongen <ongyongen2021@gmail.com>
2022-07-19 10:45:03 +0800

    add cypress config file

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,6 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   e2e: {
     // e2e options here
-    specPattern: 'cypress/tests/**/**/*.cy.{js,jsx,ts,tsx}'
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}'
   }
 })
