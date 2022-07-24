0ab6eb0b3f022519a24383ce82cd3a8ad4c67932
Carrie Zheng Jiarui <carriezhengjiarui@gmail.com>
2022-07-24 23:11:45 +0800

    Edit sticky on Task Tracker, Focus Session
    Edited sticky on Task Tracker; added "Edit" and "Save" on Goal Setting of Focus Session to be like Task Tracker

--- src/components/FocusSession/FocusSession.js
+++ src/components/FocusSession/FocusSession.js
@@ -1,11 +1,42 @@
-import React from 'react'
+import { React, useState, useEffect } from 'react'
 import Countdown from "./Countdown";
 import Title from "../Title/Title.js";
 import Header from '../Header/Header.js'
 import Music from './Music.js'
+import GoalSetting from "./GoalSetting.js"
 import './FocusSession.css'
+import { UserAuth } from '../../context/AuthContext'
+import {db} from "../../firebase.js"
+import {
+    collection,
+    query,
+    onSnapshot,
+    where,
+} from "firebase/firestore"
 
 function FocusSession () {
+    const {user}  = UserAuth();
+    const [goal, setGoal] = useState([]);
+
+    /* Retrieving words typed on goal setting sticky */
+    useEffect(() => {
+        let active = true
+        if (active == true && user.uid != null) {
+            const q = query(collection(db, "goal"), where("uid", "==", user.uid))
+            console.log("Retrieving goal")
+            onSnapshot(q, (querySnapshot) => {
+                let currentSticky = []
+                querySnapshot.forEach((doc) => {
+                    currentSticky.push({...doc.data()})
+                })
+                if (currentSticky.length != 0) {
+                    setGoal(() => currentSticky[0]['content'])
+                    console.log(currentSticky[0]['content'])
+                }
+            })
+            return () => {active = false}}
+    }, [user.uid])
+
     return (
         <div id="focus-session-container">
             <Header />
@@ -14,12 +45,8 @@ function FocusSession () {
                 <div id="timer-container">
                     <div id="text-area-container">
                         <label id="goal-label">Goal Setting</label>
-                        <textarea
-                            name="goal"
-                            placeholder='Type your goal for this session'
-                            id="goal-setting-text-area"
-                            data-cy="goal-input"
-                        />
+                        {goal.length != 0 && <GoalSetting text={goal}/>}
+                        {goal.length == 0 && <GoalSetting text={""}/>}
                     </div>
                     <div id="countdown-container">
                         <Countdown 
